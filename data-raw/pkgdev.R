97a46d16ab028daf2d01790e823fbf4c38ad0eef
mkearney <kearneymw@missouri.edu>
2019-11-04 13:51:39 -0600

    edit pkgdev

--- data-raw/pkgdev.R
+++ data-raw/pkgdev.R
@@ -2,10 +2,9 @@
 
 #usethis::create_package("congress116")
 #devtools::document()
-#usethis::use_package("tfse")
-#tfse::use_P()
-#usethis::use_package("curl")
-#usethis::use_package("jsonlite")
 #usethis::use_mit_license("Michael W. Kearney")
 #usethis::use_readme_rmd()
 #usethis::use_news_md()
+#usethis::use_git_config()
+#usethis::use_git()
+#usethis::use_github()
